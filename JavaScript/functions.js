e23d6d5ba5bf1b640990ae44f6567f3619612eb1
OmegaWarlord <60246471+OmegaWarlord@users.noreply.github.com>
2020-09-30 14:52:40 -0500

    Pre Alpha 2.1.0 build 0030-d

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,4 +1,19 @@
-// Last updated Pre Alpha 2.0.0 build 0030-c
+// Last updated Pre Alpha 2.1.0 build 0030-d
+function $(x) {return document.getElementById(x);} 
+function c(x) {return document.getElementsByClassName(x);}
+
 function goToURL(val){
     window.open(val, "_self");
 }
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $("headerTitle").style.fontSize = "20px";
+    $("frontierLogo").style.width = "2%";
+  } else {
+    $("headerTitle").style.fontSize = "60px";
+    $("frontierLogo").style.width = "10%";
+  }
+}
+
