7db62b3fd0391a2611cd0a56a6d76736eda85c87
Garrett Nalley <garrettnalley@hotmail.com>
2021-03-11 11:33:53 -0600

    WEBSITEUP

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,16 +1,20 @@
 // Last Updated Pre-Release 1.0.0 build 0219
-function goToURL(val){
+$(function(){
+  function goToURL(val){
     window.open(val, "_self");
-}
-
-$(document).scroll(function() {
-  if($(document).scrollTop() > 50 || $('html').scrollTop() > 50) {
-    $("#headerTitle").css("fontSize", "20px");
-    $("#frontierLogo").css("width", "2%");
-    $("#logo").css("width", "100px");
-  } else {
-    $("#headerTitle").css("fontSize", "60px");
-    $("#frontierLogo").css("width", "5%");
-    $("#logo").css("width", "297px");
   }
+  $(document).scroll(function() {
+    if($(document).scrollTop() > 50 || $('html').scrollTop() > 50) {
+      $("#headerTitle").css("fontSize", "20px");
+      $("#frontierLogo").css("width", "2%");
+      $("#logo").css("width", "100px");
+    } else {
+      $("#headerTitle").css("fontSize", "60px");
+      $("#frontierLogo").css("width", "5%");
+      $("#logo").css("width", "297px");
+    }
+  });
 });
+
+
+
